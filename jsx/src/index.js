65410719585ad15c7608f3bb1363481dd4af210b
Kushagra Chadha <E01870@hsc.com>
2019-07-29 10:52:24 +0530

    first excercise

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import faker from  'faker'
 
 const App= () => {
     return (
-        <div>
-            <label for="name">Enter Your Name:</label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor :'blue',color:'white'}} >
-                Submit
-            </button>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+            </div>
+            <div className="comment">
+                <a href="/" className="author">
+                    Kushagra
+                </a>
+            </div>
+            <div className="metadata">
+                <span className="date">Posted at 6:00 pm</span>
+            </div>
+            <div className="text">
+                Nice Blog
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+            </div>
+            <div className="comment">
+                <a href="/" className="author">
+                    Kushagra
+                </a>
+            </div>
+            <div className="metadata">
+                <span className="date">Posted at 6:00 pm</span>
+            </div>
+            <div className="text">
+                Nice Blog
+            </div>
         </div>
     );
 };
